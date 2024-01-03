3d2f6efcd64f642f3a33cc9f8b419d5221de3cf5
liushouping <kose146352@gmail.com>
2024-01-03 14:13:25 +0800

    Test

--- components/AppBanner.vue
+++ components/AppBanner.vue
@@ -5,11 +5,11 @@ onMounted(() => {
 </script>
 <template>
 	<div 
-	class="relative w-full h-screen bg-gray-900 flex justify-center items-center">
+	class="relative w-full h-screen bg-gray-900 flex justify-center items-center overflow-hidden">
      <video class="min-w-full min-h-full absolute z-10 object-cover bg-gray-900" src="/onmi.mp4" type="video/mp4" autoplay muted loop webkit-playsinline playsinline></video>
 		<!-- -->
 		<div 
-		class="absolute z-20 bg-repeat w-full h-screen opacity-50" 
+		class="absolute z-20 bg-repeat w-full h-[120vh] opacity-50" 
 		style="background-image: url(/dots-mini-light.png);">
 		</div>
 		<div class="absolute z-30">
