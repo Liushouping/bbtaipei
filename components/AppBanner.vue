8247f3f737ee1e2ef608b8b0275d9a920df3a4ba
liushouping <kose146352@gmail.com>
2024-01-03 12:37:05 +0800

    test

--- components/AppBanner.vue
+++ components/AppBanner.vue
@@ -5,25 +5,6 @@
 		class="min-w-full min-h-full absolute object-cover" 
 		src="/onmi.mp4" 
 		type="video/mp4" autoplay muted loop webkit-playsinline playsinline></video> -->
-		<iframe 
-		class="av_youtube_frame min-w-full min-h-full absolute object-cover" 
-		id="player_4620_1851317283_406218855" 
-		data-autoplay="1" 
-		data-videoid="2iMGoBy9dtQ" 
-		data-hd="1" 
-		data-rel="0" 
-		data-wmode="opaque" 
-		data-loop="0" 
-		data-version="3" 
-		data-autohide="1" 
-		data-color="white" 
-		data-controls="0" 
-		data-iv_load_policy="3" 
-		data-original_url="https://www.youtube.com/watch?v=2iMGoBy9dtQ" 
-		frameborder="0" allowfullscreen="" 
-		allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" title="這就是 OMNI / THIS IS OMNI【官方 PV #thisisOMNI 篇】" 
-		src="https://www.youtube.com/embed/2iMGoBy9dtQ?original_url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D2iMGoBy9dtQ&iv_load_policy=3&controls=0&color=white&autohide=1&version=3&loop=0&wmode=opaque&rel=0&hd=1&videoid=2iMGoBy9dtQ&autoplay=0&html5=1&enablejsapi=1&origin=https%3A%2F%2Fwww.omni-taipei.com&widgetid=1&showinfo=0" 
-		data-gtm-yt-inspected-15="true"></iframe>
 
 		<iframe 
 		    class="w-full aspect-video av_youtube_frame min-h-full absolute object-cover" 
