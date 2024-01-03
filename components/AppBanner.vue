20750eb039960727e3033c5dba6951befa519188
liushouping <kose146352@gmail.com>
2024-01-03 12:48:06 +0800

    test

--- components/AppBanner.vue
+++ components/AppBanner.vue
@@ -1,53 +1,45 @@
 <script setup>
 onMounted(() => {
-document.getElementById('player_4620_1851317283_406218855').addEventListener('load', function() {
-    // 獲取嵌入的播放器
-    var player = new YT.Player('player_4620_1851317283_406218855', {
-        events: {
-            'onReady': function(event) {
-                // 監聽播放結束事件
-                event.target.addEventListener('onStateChange', function(state) {
-                    // 如果影片結束，重新播放
-                    if (state.data === YT.PlayerState.ENDED) {
-                        event.target.playVideo();
-                    }
-                });
-            }
+	const player = new YT.Player('player_4620_1851317283_406218855', {
+    events: {
+      'onReady': function(event) {
+        event.target.playVideo(); // 自动播放
+      },
+      'onStateChange': function(event) {
+        // 在视频结束时重新播放
+        if (event.data === YT.PlayerState.ENDED) {
+          event.target.playVideo();
         }
-    });
-});
+      }
+    }
+  });
 });
 </script>
 <template>
 	<div 
 	class="relative w-full h-screen bg-gray-900 flex justify-center items-center">
-<!-- 		<video 
-		class="min-w-full min-h-full absolute object-cover" 
-		src="/onmi.mp4" 
-		type="video/mp4" autoplay muted loop webkit-playsinline playsinline></video> -->
-
-		<iframe 
-		    class="w-full aspect-video av_youtube_frame min-h-full absolute object-cover" 
-		    id="player_4620_1851317283_406218855" 
-		    data-autoplay="1"
-		    data-videoid="2iMGoBy9dtQ" 
-		    data-hd="1" 
-		    data-rel="0" 
-		    data-wmode="opaque" 
-		    data-loop="0" 
-		    data-version="3" 
-		    data-autohide="1" 
-		    data-color="white" 
-		    data-controls="0" 
-		    data-iv_load_policy="3" 
-		    data-original_url="https://www.youtube.com/watch?v=2iMGoBy9dtQ" 
-		    frameborder="0" 
-		    allowfullscreen="" 
-		    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-		    title="這就是 OMNI / THIS IS OMNI【官方 PV #thisisOMNI 篇】" 
-		    src="https://www.youtube.com/embed/2iMGoBy9dtQ?original_url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D2iMGoBy9dtQ&iv_load_policy=3&controls=0&color=white&autohide=1&version=3&loop=0&wmode=opaque&rel=0&hd=1&videoid=2iMGoBy9dtQ&autoplay=1&mute=1&html5=1&enablejsapi=1&origin=https%3A%2F%2Fwww.omni-taipei.com&widgetid=1&showinfo=0" 
-		    data-gtm-yt-inspected-15="true">
-		</iframe>
+		<iframe
+      class="w-full aspect-video av_youtube_frame min-h-full absolute object-cover"
+      id="player_4620_1851317283_406218855"
+      data-autoplay="1"
+      data-videoid="2iMGoBy9dtQ"
+      data-hd="1"
+      data-rel="0"
+      data-wmode="opaque"
+      data-loop="0"
+      data-version="3"
+      data-autohide="1"
+      data-color="white"
+      data-controls="0"
+      data-iv_load_policy="3"
+      data-original_url="https://www.youtube.com/watch?v=2iMGoBy9dtQ"
+      frameborder="0"
+      allowfullscreen=""
+      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+      title="這就是 OMNI / THIS IS OMNI【官方 PV #thisisOMNI 篇】"
+      src="https://www.youtube.com/embed/2iMGoBy9dtQ?original_url=https%3A%2F%2Fwww.youtube.com%2Fwatch%3Fv%3D2iMGoBy9dtQ&iv_load_policy=3&controls=0&color=white&autohide=1&version=3&loop=0&wmode=opaque&rel=0&hd=1&videoid=2iMGoBy9dtQ&autoplay=1&mute=1&html5=1&enablejsapi=1&origin=https%3A%2F%2Fwww.omni-taipei.com&widgetid=1&showinfo=0"
+      data-gtm-yt-inspected-15="true"
+    ></iframe>
 
 		<!-- -->
 		<div 
