2af0ce88a6201bfd2299f471f3bf22a9809950b6
liushouping <kose146352@gmail.com>
2024-01-04 16:09:49 +0800

    Test

--- components/AppToexposure.vue
+++ components/AppToexposure.vue
@@ -1,5 +1,11 @@
 <template>
-	<div>
-		exposure
+	<div 
+	class="relative bg-gray-900 h-[1200px] bg-cover bg-center flex justify-center"
+	style="background-image: url(/bg.jpg);">
+		<div class="absolute z-10 bg-gray-900/70 w-full h-full">
+		</div>
+		<div class="max-w-[80%] w-full h-full py-36 z-20">
+			....
+		</div>
 	</div>
 </template>
