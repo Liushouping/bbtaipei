3ee82bb3feb331b6daf0c5ad48aefeba4dd06829
liushouping <kose146352@gmail.com>
2024-01-16 16:08:28 +0800

    test

--- components/App404.vue
+++ components/App404.vue
@@ -5,7 +5,7 @@
 			<p
 			class="text-gray-50 font-bold text-[148px] sm:text-[172px] tracking-[16px]">404</p>
 			
-			<p class="font-light text-[36px] text-gray-50 tracking-[2px]">Page not found</p>
+			<p class="font-light text-[36px] text-gray-50 tracking-[2px]">Page not found ...</p>
 			<NuxtLink 
 			to="/" 
 			class="bg-gray-800 text-[14px] px-6 py-2 text-gray-50 rounded-lg flex flex-row items-center tracking-[1px] hover:bg-gray-700 transition-colors mt-8">
