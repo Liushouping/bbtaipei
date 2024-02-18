4475d808e515eb5affbde4b69757ffbe9bd3887a
liushouping <kose146352@gmail.com>
2024-02-18 22:50:07 +0800

    Test

--- components/App404.vue
+++ components/App404.vue
@@ -7,9 +7,16 @@ useHead({
 <template>
 	<div class="w-full h-screen bg-gray-950 pt-48 pb-24">
 		<div class="flex flex-col justift-center items-center">
-			<p class="text-[#F0B90B] font-bold text-[124px] sm:text-[172px] tracking-[12px]">Oops!</p>
-			<p class="text-[14px] text-gray-50 mb-2">404 Error</p>
-			<p class="text-[#F0B90B] font-light text-[24px] tracking-[2px]">We can’t find that page ...</p>
+			<p class="bg-clip-text text-transparent bg-gradient-to-b from-[#F0B90B] to-gray-950 font-bold text-[124px] sm:text-[172px] tracking-[12px]">Oops!</p>
+
+			<span class="flex flex-row space-x-1 justift-center items-center mb-2">
+				<p class="text-[14px] bg-clip-text text-transparent bg-gradient-to-b from-gray-50 to-gray-950">404 Error</p>
+<!-- 				<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 text-gray-50">
+				  <path stroke-linecap="round" stroke-linejoin="round" d="M15.182 16.318A4.486 4.486 0 0 0 12.016 15a4.486 4.486 0 0 0-3.198 1.318M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0ZM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75Zm-.375 0h.008v.015h-.008V9.75Z" />
+				</svg> -->
+			</span>
+
+			<p class="bg-clip-text text-transparent bg-gradient-to-b from-[#F0B90B] to-gray-950 font-light text-[24px] tracking-[2px]">We can’t find that page ...</p>
 			<div class="flex flex-row space-x-4">
 				<NuxtLink
 				to="/"
