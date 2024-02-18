65e4a679003a3957eee48790e9d6ef6aa604f5e9
liushouping <kose146352@gmail.com>
2024-02-18 21:17:48 +0800

    Test

--- components/App404.vue
+++ components/App404.vue
@@ -7,8 +7,9 @@ useHead({
 <template>
 	<div class="w-full h-screen bg-gray-950 pt-48 pb-24">
 		<div class="flex flex-col justift-center items-center">
-			<p class="text-[#F0B90B] font-bold text-[148px] sm:text-[172px] tracking-[16px]">404</p>
-			<p class="text-[#F0B90B] font-light text-[36px] tracking-[2px]">We can’t find that page ...</p>
+			<p class="text-[#F0B90B] font-bold text-[124px] sm:text-[172px] tracking-[12px]">Oops!</p>
+			<p class="text-[14px] text-gray-50 mb-2">404 Error</p>
+			<p class="text-[#F0B90B] font-light text-[24px] tracking-[2px]">We can’t find that page ...</p>
 			<NuxtLink
 			to="/"
 			class="bg-gray-800 text-[12px] px-6 py-2 font-light text-gray-50 rounded-lg flex flex-row items-center tracking-[1px] hover:bg-gray-700 transition-colors mt-8">
